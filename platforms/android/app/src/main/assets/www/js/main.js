669585cba0e97b23fa3d1f9af6a1f7580ad573f3
Oskar Lewandowski <oskar55514@wp.pl>
2020-04-30 16:40:49 +0200

    Dodanie aplikacji mobilnej do repozytorium

---
+++ platforms/android/app/src/main/assets/www/js/main.js
@@ -0,0 +1,23 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+  $('#sidebarCollapse').on('click', function () {
+	$('#overlay').toggleClass('overlay');
+});
+
+})(jQuery);
